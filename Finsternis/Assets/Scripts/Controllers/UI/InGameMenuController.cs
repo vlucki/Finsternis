dc953112831afd3dc1eb211d3f6b690add8c4b9f
Victor Lucki <LuciusSixPercent@users.noreply.github.com>
2016-08-29 12:03:10 -0300

    InGame Menu transition
    - Implemented Open and Close funcionality to menu (unable to test right now, so there may be a few issues)
    
    - Documented a couple methods;

--- Finsternis/Assets/Scripts/Controllers/UI/InGameMenuController.cs
+++ Finsternis/Assets/Scripts/Controllers/UI/InGameMenuController.cs
@@ -1,4 +1,4 @@
-﻿namespace Finsternis
+namespace Finsternis
 {
     using UnityEngine;
     using System.Collections;
@@ -18,8 +18,14 @@
         private CanvasGroup group;
         private List<Button> options;
         private Circle menuBounds;
-        private bool togglingMenu;
+        private bool transitioning;
         private EventSystem evtSys;
+        private float targetPercentage;
+
+        public UnityEvent OnMenuOpen;
+        public UnityEvent OnMenuClose;
+
+        private UnityEvent OnTransitionFinished;
 
         void Awake()
         {
@@ -34,47 +40,76 @@
                 this.group = GetComponent<CanvasGroup>();
                 this.menuBounds = new Circle(GetComponent<RectTransform>().sizeDelta.Min()/2, GetComponent<RectTransform>().anchoredPosition);
                 this.options = new List<Button>();
+                this.OnMenuClose.AddListener(() => gameObject.SetActive(false));
+                
                 GetComponentsInChildren<Button>(options);
                 if (options.Count <= 0)
                     Log.Warn("Not a single option found on the menu.");
-                else
-                    PositionButtons();
-                
             }
         }
 
+        void OnEnable(){
+          Open();
+        }
+
+        /// <summary>
+        /// Sets up everything in order for the menu to open (animations, effects, callbacks)
+        /// </summary>
         public void Open()
         {
-
+            this.OnMenuOpen.Invoke();
+            this.targetPercentage = 1;
+            this.OnTransitionFinished = null;
+            if(!transitioning)
+                Timing.RunCoroutine(_ToggleMenu());
         }
-
+        
+        /// <summary>
+        /// Sets up everything in order for the menu to close (animations, effects, callbacks)
+        /// </summary>
         public void Close()
         {
-
+            this.targetPercentage = 0;
+            this.OnTransitionFinished = this.OnMenuClose;
+            if(!transitioning)
+                Timing.RunCoroutine(_ToggleMenu());
         }
 
-        private void _ToggleMenu(bool opening)
+        /// <summary>
+        /// Animates the menu, interpolating it's current and target state (eg. from Closed to Opened)
+        /// </summary>
+        private void _ToggleMenu()
         {
-            float targetPercentage = opening ? 1 : 0;
-            float currentPercentage = 1 - targetPercentage;
+            transitioning = true;
+            float currentPercentage = 1 - this.targetPercentage;
             do
             {
                 PositionButtons(currentPercentage);
                 currentPercentage = Mathf.Lerp(currentPercentage, targetPercentage, 0.2f);
             }
             while (!Mathf.Approximately(currentPercentage, targetPercentage));
+
+            PositionButtons(targetPercentage); //call once more to avoid precision errors
+            
+            transitioning = false;
+            
+            if(this.OnTransitionFinished) this.OnTransitionFinished.Invoke();
         }
 
+        /// <summary>
+        /// Position every button within the menu in a circular fashion.
+        /// </summary>
+        /// <param name="percentage">How far from the center should the buttons be? (0 = centered, 1 = the menu radius. </param>
         private void PositionButtons(float percentage = 1)
         {
             float angleBetweenOptions = -360 / options.Count;
-            Vector2 optionPosition = menuBounds.center + Vector2.up * menuBounds.radius;
+            Vector2 optionPosition = this.menuBounds.center + Vector2.up * this.menuBounds.radius;
             for(int index = 0; index < options.Count; index++)
             {
-                options[index].GetComponent<RectTransform>().anchoredPosition = optionPosition;
+                this.options[index].GetComponent<RectTransform>().anchoredPosition = optionPosition;
                 var dir = optionPosition - menuBounds.center;
                 dir = dir.Rotate(angleBetweenOptions);
-                optionPosition = dir.normalized * menuBounds.radius * percentage;
+                optionPosition = dir.normalized * this.menuBounds.radius * percentage;
             }
         }
     }
